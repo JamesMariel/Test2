997685a2b4f15d19cd9f87eec956d80d6532c6c7
Jameshak <76936549+Jameshak@users.noreply.github.com>
2024-02-17 00:53:36 +0630

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: ' Jan 16, 2024',
+  birthdate: ' March 16, 2024',
   name: '  Aye Charm Thu'
 };
 
